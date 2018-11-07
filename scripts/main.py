ec44c389cc7dcf8cdd13f5b26b63ab1372a54249
Zachary Amiton <zackaryjoey3@hotmail.com>
2018-11-08 01:29:37 +0530

    added frequency file
    - works okay for now, the data set is an absolute mess—really should've
    decided on some norms for punctuation!!!!
    - might sanitize data to delete all punctuation, but that would
    sacrifice my poor sweet emote bois...hmmmmm...could use regex to delete
    all punctuation if message doesn't contain [•-•] or something idk
    - also in future, switching encoding...is ugh, so many Âø nonsense
    characters (rip emotes with random unicode strings in them)

--- scripts/main.py
+++ scripts/main.py
@@ -1,48 +1,41 @@
 import os
 import json
 
-path = "/Users/zackamiton/GitHub/MessageAnalysis/data/Sanitized Files"
+path = "/Users/zackamiton/GitHub/MessageAnalysis/data/Safe Files"
 directory = os.listdir(path)
 
-read_file = open(path + "/COMBINED_MESSAGE_OUTPUT.txt", "r")
-write_file = open(path + "/SORTED_MESSAGE_OUTPUT.txt", "w")
+read_file = open(path + "/SORTED_MESSAGE_OUTPUT.txt", "r")
+write_file = open(path + "/SORTED_MESSAGE_OUTPUT_2.txt")
+freq_file = open(path + "/FREQUENCY_MESSAGE_OUTPUT.txt", "r+")
 
-i = 0
-words = []
+def read_in_and_sort():
+    words = []
 
-for line in read_file.readlines():
-    words.append(line)
-    i+=1
+    for line in read_file.readlines():
+        words.append(line.lower())
 
-words.sort()
+    words.sort()
 
-for s in words:
-    write_file.write(s)
+    for w in words:
+        write_file.write(w)
+
+def create_frequency_from_read_file():
+    count = 0
+    l = read_file.readline().rstrip("\n")
+
+    for line in read_file.readlines():
+        line = line.rstrip("\n")
+        if l != line:
+            freq_file.write(l + ", " + str(count) + "\n")
+            count = 0
+
+        count+=1
+        l = line
+
 
 read_file.close()
+freq_file.close()
 write_file.close()
-
-
-
-#
-# for f in directory:
-#     os.rename(os.path.join(path, f), os.path.join(path, "MESSAGE_" + str(i)+'.json'))
-#     i = i+1
-#
-# for files in directory:
-#     with open(files) as f:
-#         data = json.load(f)
-#
-#     for m in data["messages"]:
-#         if "sender_name" in m and "content" in m:
-#             if m["sender_name"] == "Zachary Amiton":
-#                 line = m["content"].encode('utf-8')
-#                 write_file.write(line + "\n")
-
-
-
-
-
 
 if __name__ == '__main__':
     pass
